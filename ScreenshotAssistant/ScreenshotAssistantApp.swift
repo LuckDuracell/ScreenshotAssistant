c4e8e4efcc07a8040d27831d58569c10848ac3dc
LuckDev <lukedru@yahoo.com>
2023-02-22 11:28:50 -0800

    Initial Commit

---
+++ ScreenshotAssistant/ScreenshotAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotAssistantApp.swift
+//  ScreenshotAssistant
+//
+//  Created by Luke Drushell on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenshotAssistantUITests/ScreenshotAssistantUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ScreenshotAssistantUITestsLaunchTests.swift
+//  ScreenshotAssistantUITests
+//
+//  Created by Luke Drushell on 2/22/23.
+//
+
+import XCTest
+
+final class ScreenshotAssistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
